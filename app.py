2487007b296155d80f7e32b7e07779ad54324a48
Harshal R Patel <patelharshu6699@gmail.com>
2020-02-02 06:26:11 +0530

    Initial-commit Working : Search

--- app.py
+++ app.py
@@ -12,13 +12,13 @@ def index():
 def property():
     location = request.form['location_list']
     property_type = request.form['property_types']
-    bed_rooms = int(str(request.form['bed_rooms']).split('+')[0])
-    bath_rooms = int(str(request.form['bath_rooms']).split('+')[0])
+    bed_rooms = str(request.form['bed_rooms'])
+    bath_rooms = str(request.form['bath_rooms'])
     range_min = float(request.form['min_val']) * 100000
     range_max = float(request.form['max_val']) * 100000
 
     print(location, property_type, bed_rooms, bath_rooms, range_min, range_max)
-    query = { '$and': [ { "price": { '$gt': range_min} }, { "price": { '$lt': range_max  } }, {"location": location}, {"type": property_type}, {"bedrooms": {'$gte': bed_rooms}}, {"bathrooms": {'$gte': bath_rooms}} ] }
+    query = { '$and': [ { "price": { '$gt': range_min} }, { "price": { '$lt': range_max  } }, {"location": location}, {"type": property_type}, {"bedrooms": {'$gte': int(str(request.form['bed_rooms']).split('+')[0])}}, {"bathrooms": {'$gte': int(str(request.form['bath_rooms']).split('+')[0])}} ] }
     result = config.collection.find(query)
     count = 0
     property_list = []
@@ -26,8 +26,9 @@ def property():
         count += 1
         property_list.append(x)
 
+    vals = [location, property_type, range_min, range_max, bed_rooms, bath_rooms]
 
-        return render_template('property.html', property_count=count, property_list=property_list)
+    return render_template('property.html', property_count=count, property_list=property_list, val=vals)
 
 
 @app.route('/property', methods=['GET'])
